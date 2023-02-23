d8861ef32ee2e143b4631c24fa7ca8589b5580ce
krishnas6795 <krishnas6795@gmail.com>
2023-02-23 22:57:14 +0530

    Added templates and static.

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-from flask import Flask, redirect, url_for, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return "Hello World"
+# @app.route('/')
+# def hello_world():
+#     return "Hello World"
 
-@app.route('/hello/<name>')
-def hello_name(name):
-    return "Hello %s!" %name
+# @app.route('/hello/<name>')
+# def hello_name(name):
+#     return "Hello %s!" %name
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
