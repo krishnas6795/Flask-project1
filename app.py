0f6e69172daaeac272f0f0a0269747c401437c49
krishnas6795 <krishnas6795@gmail.com>
2023-02-24 01:14:03 +0530

    Added database file and app code.

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
+import sqlite3
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# @app.route('/')
-# def hello_world():
-#     return "Hello World"
-
-# @app.route('/hello/<name>')
-# def hello_name(name):
-#     return "Hello %s!" %name
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
 
 if __name__ == '__main__':
     app.run(debug=True)
